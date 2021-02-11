f3ca814a04ea57702b0c68c3a7817d578eadb238
Russell <russelldev@protonmail.com>
2021-02-11 23:34:11 +0000

    Add JSON notes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const addBtn = document.getElementById('add');
-
+const notes = JSON.parse(localStorage.getItem('notes'));
 addBtn.addEventListener('click', () => addNewNote());
-
 function addNewNote(text = '') {
     const note = document.createElement('div');
     note.classList.add('note');
